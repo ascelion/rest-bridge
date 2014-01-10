e8fe4ba98b374b5baae29d9fbe6d920b61749eb5
pappy <pa314159@users.noreply.github.com>
2014-01-10 04:58:04 +0200

    defined media type for Methods

--- tests/application/src/main/java/ascelion/rest/bridge/web/Methods.java
+++ tests/application/src/main/java/ascelion/rest/bridge/web/Methods.java
@@ -1,6 +1,7 @@
 
 package ascelion.rest.bridge.web;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.HEAD;
@@ -8,8 +9,12 @@ import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path( "methods" )
+@Produces( MediaType.APPLICATION_JSON )
+@Consumes( MediaType.APPLICATION_JSON )
 public interface Methods
 {
 
