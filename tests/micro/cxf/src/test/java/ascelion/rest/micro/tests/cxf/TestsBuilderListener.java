51f332dcb16f08255fcac3facc0e1a97ba1db163
pappy <pa314159@users.noreply.github.com>
2019-01-08 13:38:32 +0200

    provide a default content type - what does the spec say?

---
+++ tests/micro/cxf/src/test/java/ascelion/rest/micro/tests/cxf/TestsBuilderListener.java
@@ -0,0 +1,15 @@
+
+package ascelion.rest.micro.tests.cxf;
+
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.eclipse.microprofile.rest.client.spi.RestClientBuilderListener;
+
+public class TestsBuilderListener implements RestClientBuilderListener
+{
+
+	@Override
+	public void onNewBuilder( RestClientBuilder bld )
+	{
+	}
+
+}
