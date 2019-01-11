240744274177ccc54f4115a61dff8d7837b5a360
pappy <pa314159@users.noreply.github.com>
2019-01-11 16:17:44 +0200

    removed wiremock-standalone

--- tests/shared/src/main/java/ascelion/rest/micro/tests/shared/TestsExtension.java
+++ tests/shared/src/main/java/ascelion/rest/micro/tests/shared/TestsExtension.java
@@ -1,6 +1,9 @@
 
 package ascelion.rest.micro.tests.shared;
 
+import ascelion.rest.bridge.tests.api.SLF4JHandler;
+
+import ch.qos.logback.core.CoreConstants;
 import org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor;
 import org.jboss.arquillian.core.spi.LoadableExtension;
 
@@ -8,6 +11,7 @@ public class TestsExtension implements LoadableExtension
 {
 
 	static {
+		System.setProperty( CoreConstants.DISABLE_SERVLET_CONTAINER_INITIALIZER_KEY, "true" );
 		SLF4JHandler.install();
 	}
 
