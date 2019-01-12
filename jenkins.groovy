ff5feaf960672432fce7eb06ca3e0aef18333cab
pappy <pa314159@users.noreply.github.com>
2019-01-12 14:01:09 +0200

    new standalone pipeline

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,3 +1,41 @@
-gradle {
-	timeout = 30
+pipeline {
+	agent any
+	options {
+		disableConcurrentBuilds()
+		timeout time: 15, unit: 'MINUTES'
+		buildDiscarder logRotator( daysToKeepStr: "14" )
+	}
+	tools {
+		jdk 'J8'
+	}
+	environment {
+		JENKINS_NODE_COOKIE = 'dontkillme'
+	}
+
+	stages {
+		stage('Build') {
+			steps {
+				sh "chmod +x gradlew"
+				sh "./gradlew cV clean build -xcheck"
+			}
+		}
+		stage('Checks') {
+			steps {
+				sh "./gradlew check --continue"
+				junit allowEmptyResults: true, testResults: "**/TEST-*.xml"
+				step([$class: 'Publisher', reportFilenamePattern: '**/testng-results.xml'])
+			}
+		}
+		stage('Publish') {
+			steps {
+				sh "./gradlew publishLocal publish -xcheck "
+			}
+		}
+		stage('Archive') {
+			steps {
+				archiveArtifacts fingerprint: true,
+				artifacts: '**/build/publications/*/pom-*.xml, **/build/libs/*.jar'
+			}
+		}
+	}
 }
