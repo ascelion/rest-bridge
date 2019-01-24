40d404e4fdd57e1a1f851d3cf7ae63e2db1fb9d5
pappy <pa314159@users.noreply.github.com>
2019-01-24 19:41:13 +0200

    trying to solve missing Content-Type

---
+++ micro/src/arq/java/ascelion/rest/micro/tests/GetAnyTest.java
@@ -0,0 +1,100 @@
+
+package ascelion.rest.micro.tests;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.core.Response;
+
+import static java.util.Collections.singletonMap;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.eclipse.microprofile.rest.client.tck.WiremockArquillianTest;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.testng.annotations.Test;
+
+public class GetAnyTest extends WiremockArquillianTest
+{
+
+	@Path( "" )
+	@RegisterRestClient( baseUri = "http://localhost" )
+	public interface GetAny
+	{
+
+		@GET
+		@Path( "map" )
+		Map<String, String> getMap();
+
+		@GET
+		@Path( "string" )
+		String getString();
+	}
+
+	static public class AbortResponse implements ClientRequestFilter
+	{
+
+		static Object body;
+
+		@Override
+		public void filter( ClientRequestContext ctx ) throws IOException
+		{
+			assertThat( body, notNullValue() );
+
+			ctx.abortWith( Response.ok( body ).build() );
+		}
+	}
+
+	@Inject
+	@RestClient
+	private GetAny client;
+
+	@Deployment
+	public static WebArchive createDeployment()
+	{
+		final StringAsset mpConfig = new StringAsset( GetAny.class.getName() + "/mp-rest/providers=" + AbortResponse.class.getName() );
+
+		final JavaArchive jar = ShrinkWrap.create( JavaArchive.class, "GetMap.jar" )
+			.addClasses( GetAnyTest.class, WiremockArquillianTest.class )
+			.addAsManifestResource( mpConfig, "microprofile-config.properties" )
+			.addAsManifestResource( EmptyAsset.INSTANCE, "beans.xml" );
+
+		return ShrinkWrap.create( WebArchive.class, "GetMap.war" )
+			.addAsLibraries( jar )
+			.addAsWebInfResource( EmptyAsset.INSTANCE, "beans.xml" );
+	}
+
+	@Test
+	public void getString()
+	{
+		AbortResponse.body = "HIHIHIHI";
+
+		final String body = this.client.getString();
+
+		assertThat( body, equalTo( AbortResponse.body ) );
+	}
+
+	@Test
+	public void getMap()
+	{
+		AbortResponse.body = singletonMap( "key", "value" );
+
+		final Map<String, String> map = this.client.getMap();
+
+		assertThat( map, notNullValue() );
+		assertThat( map.get( "key" ), equalTo( "value" ) );
+	}
+}
--- tests/shared/src/main/java/ascelion/rest/micro/tests/shared/TestsBuilderListener.java
+++ tests/shared/src/main/java/ascelion/rest/micro/tests/shared/TestsBuilderListener.java
@@ -2,13 +2,16 @@
 package ascelion.rest.micro.tests.shared;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientResponseContext;
 import javax.ws.rs.client.ClientResponseFilter;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import ascelion.rest.bridge.client.RestClient;
 import ascelion.rest.bridge.etc.RestClientTrace;
 
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
@@ -23,8 +26,27 @@ public class TestsBuilderListener implements RestClientBuilderListener
 		@Override
 		public void filter( ClientRequestContext req, ClientResponseContext rsp ) throws IOException
 		{
-			if( rsp.getMediaType() == null ) {
-				rsp.getHeaders().add( HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON );
+			if( rsp.getStatus() == Response.Status.NO_CONTENT.getStatusCode() ) {
+				return;
+			}
+
+			final Method m = RestClient.invokedMethod();
+
+			if( m != null && rsp.getMediaType() == null && rsp.getStatus() != Response.Status.NO_CONTENT.getStatusCode() ) {
+				final String type = m.getReturnType().getSimpleName().toLowerCase();
+
+				switch( type ) {
+					case "response":
+					case "void":
+					break;
+
+					case "string":
+						rsp.getHeaders().add( HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN );
+					break;
+
+					default:
+						rsp.getHeaders().add( HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON );
+				}
 			}
 		}
 	}
@@ -32,11 +54,9 @@ public class TestsBuilderListener implements RestClientBuilderListener
 	@Override
 	public void onNewBuilder( RestClientBuilder b )
 	{
-//		b.register( new ContentTypeResponseFilter(), Integer.MAX_VALUE );
+		b.register( new ContentTypeResponseFilter(), Integer.MAX_VALUE );
 		b.register( new RestClientTrace(), Integer.MAX_VALUE - 1 );
 		b.register( new WildcardBodyWriter<>(), Integer.MAX_VALUE );
-
-//		b.property( RestClientProperties.DEFAULT_CONTENT_TYPE, MediaType.APPLICATION_JSON_TYPE );
 	}
 
 }
